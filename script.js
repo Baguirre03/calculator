c0687be2f751ea832e983102b63e8e891cc9e7d4
Ben Aguirre <ben.aguirre03@gmail.com>
2022-11-10 15:09:51 -0600

    Progress update: 	Query Selectors

--- script.js
+++ script.js
@@ -35,3 +35,22 @@ const operate = function(operator, a, b) {
     }
 }
 
+//Selected all buttons and output section
+const one = document.querySelector('#one')
+const two = document.querySelector('#two')
+const three = document.querySelector('#three')
+const four = document.querySelector('#four')
+const five = document.querySelector('#five')
+const six = document.querySelector('#six')
+const seven = document.querySelector('#seven')
+const eight = document.querySelector('#eight')
+const nine = document.querySelector('#nine')
+const zero = document.querySelector('#zero')
+const period = document.querySelector('#period')
+const equals = document.querySelector('#equals')
+
+const output = document.querySelector('.output')
+
+const buttons = document.querySelectorAll('button')
+
+let outputChange = buttons.addEventListener('click', () => )
